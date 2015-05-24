8792d11b43b2443b47bce337343f7fd57afbe390
razorahul <razorahul@gmail.com>
2015-05-24 21:50:23 +0530

    initial project version

---
+++ backend/src/main/java/org/vertipac/university/backend/App.java
@@ -0,0 +1,13 @@
+package org.vertipac.university.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
